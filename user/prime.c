1a16593967c25ebdae2f70f84afc3ad23563cac7
SamuelSlizik <129870569+SamuelSlizik@users.noreply.github.com>
2024-10-06 14:12:29 +0200

    Update prime.c

--- user/prime.c
+++ user/prime.c
@@ -11,7 +11,7 @@ main(int argc, char *argv[])
     bool main = true;
     int childPipe[2];
     int parentPipe[2];
-    int buffer[2];
+    int buffer;
     int number = -1;
 
     if (pipe(childPipe) == -1) {
@@ -39,21 +39,19 @@ main(int argc, char *argv[])
         }
 
         read(parentPipe[0], &buffer, sizeof(number));
-        printf("buffer number is %d\n", buffer[0]);
 
-        if (buffer[0] == -1) {
-            printf("about to stop\n");
+        if (buffer == -1) {
             if (pid > 0) {
-                write(childPipe[1], &number, sizeof(number));
+                write(childPipe[1], &buffer, sizeof(number));
                 wait(NULL);
                 close(childPipe[1]);
             }
             close(parentPipe[0]);
             exit(0);
         } else if (number == -1) {
-            number = buffer[0];
-        } else if (buffer[0] % number != 0) {
-            printf("not divisible\n");
+            number = buffer;
+            printf("prime: %d\n", number);
+        } else if (buffer % number != 0) {
             if (pid == 0) {
                 if (pipe(childPipe) == -1) {
                     printf("Pipe error\n");
@@ -67,16 +65,15 @@ main(int argc, char *argv[])
                     exit(1);
                 } else if (pid > 0) {
                     close(childPipe[0]);
-                    write(childPipe[1], &number, sizeof(number));
+                    write(childPipe[1], &buffer, sizeof(number));
                 } else {
                     close(childPipe[1]);
                     parentPipe[0] = childPipe[0];
                     parentPipe[1] = 0;
-                    number = buffer[0];
-                    printf("prime: %d\n", number);
+                    number = -1;
                 }
             } else {
-                write(childPipe[1], &number, sizeof(number));
+                write(childPipe[1], &buffer, sizeof(number));
             }
         }
     }
@@ -85,10 +82,8 @@ main(int argc, char *argv[])
         for (number = 2; number <= 20; number += 1) {
             write(childPipe[1], &number, sizeof(number));
             sleep(1);
-            printf("wrote %d\n", number);
         }
         number = -1;
-        printf("wrote %d\n", number);
         write(childPipe[1], &number, sizeof(number));
         wait(NULL);
     } else {
